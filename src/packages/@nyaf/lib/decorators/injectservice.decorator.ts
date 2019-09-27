e99404064a573048759e58f3e24cb7e26cb0e29c
Joerg Krause <joerg@krause.net>
2019-09-27 20:19:04 +0200

    Update missing enum

--- src/packages/@nyaf/lib/decorators/injectservice.decorator.ts
+++ src/packages/@nyaf/lib/decorators/injectservice.decorator.ts
@@ -3,7 +3,8 @@ import { ServiceType } from '../types/common';
 export function InjectService<T>(name: string, type: ServiceType<T>) {
   return function(target: any) {
     // closure to create a singleton
-    const t = new type();
+    const t = {};
+    t[name] = new type();
     Object.defineProperty(target, 'service', {
       get: function() {
         return t;
--- src/packages/@nyaf/lib/index.ts
+++ src/packages/@nyaf/lib/index.ts
@@ -6,5 +6,6 @@ export * from './decorators/properties.decorator';
 export * from './services/observer.service';
 export * from './types/common';
 export * from './components/base.component';
+export * from './components/lifecycle.enum';
 import JSX from './components/jsx';
 export default JSX;
