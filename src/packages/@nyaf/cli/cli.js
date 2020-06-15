5f25f9255f7d315b1a07bdb2e51d3882bbc3969c
Joerg Krause <joerg@krause.net>
2020-06-16 00:08:34 +0200

    CLI

--- src/packages/@nyaf/cli/cli.js
+++ src/packages/@nyaf/cli/cli.js
@@ -2,3 +2,32 @@
 const [, , ...args] = process.argv;
 
 console.log('Handle arguments');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+
+const QUESTIONS = [
+  {
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: CHOICES
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate: function (input) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Project name may only include letters, numbers, underscores and hashes.';
+    }
+  }
+];
+
+
+inquirer.prompt(QUESTIONS)
+  .then(answers => {
+    console.log(answers);
+});
