2ef157417bb859a91bba1da2f1abf8c6bfeee346
Joerg Krause <joerg@krause.net>
2020-09-04 21:39:27 +0200

    Extracted exprimental code and move homepage

--- src/demos/features/components/pages/lib-demo/components/smart.component.tsx
+++
@@ -1,7 +0,0 @@
-changeOtherButton(e: CustomEvent) {
-  this.querySelector<ButtonComponent>('app-button[data-demo-button]').setData('text', Math.round(Math.random() * 100));
-}
-
-changeOtherButtonToZeros(e: CustomEvent) {
-  this.querySelector<ButtonComponent>('app-button[data-demo-button]').setZero();
-}
--- src/demos/features/components/pages/store-demo/actions/global.actions.ts
+++
@@ -1,1 +0,0 @@
-export const SETTAB = 'SETTAB';
